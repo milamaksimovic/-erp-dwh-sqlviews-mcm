1e8f724837676fa39c98b571edff45571cea1ac6
RobertWendlerMS <Robert.Wendler@marleyspoon.com>
2022-04-30 19:02:51 +0200

    added Sales Order Header Extension SQL View

---
+++ views/Sales Order Header Extension.sql
@@ -0,0 +1,19 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE OR ALTER VIEW [dwh].[$(CompanyName)$Sales Order Header Extension] AS
+SELECT [No_]
+      ,[Checked Out]
+      ,[Checked Out Timestamp]
+      ,[Production Line No_]
+      ,[Production Line Description]
+      ,[Checked Out Date]
+      ,[Checked Out Hour]
+  FROM [dbo].[$(CompanyName)$Sales Order Header Extension] 
+  WITH (NOLOCK)
+GO
+
+
