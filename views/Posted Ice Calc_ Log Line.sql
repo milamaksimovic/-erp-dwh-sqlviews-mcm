cc1e392806899c8cdcc11c95169f8f54511c2394
RobertWendlerMS <Robert.Wendler@marleyspoon.com>
2022-12-27 18:02:18 +0100

    ERP-1586 Added new views for Ice Calculation

---
+++ views/Posted Ice Calc_ Log Line.sql
@@ -0,0 +1,32 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE OR ALTER VIEW [dwh].[$(CompanyName)$Posted Ice Calc_ Log Line] AS
+SELECT [Sales Shipment No_]
+      ,[Segment No_]
+      ,[Segment Date From]
+      ,[Segment Date To]
+      ,[Segment Time From]
+      ,[Segment Time To]
+      ,[Segment Transit Time (hrs)]
+      ,[Chilled]
+      ,[Segment Temperature]
+      ,[Segment Transit Time Error]
+      ,[Description]
+      ,[Forecast Created Date_Time]
+      ,[Forecast Entry Date_Time]
+      ,[Forecast From Date_Time]
+      ,[Forecast To Date_Time]
+      ,[Forecast Source]
+      ,[Segment Country_Region Code]
+      ,[Segment Post Code]
+  FROM [dbo].[$(CompanyName)$Posted Ice Calc_ Log Line]
+  WITH (NOLOCK)
+GO
+
+
+
+
